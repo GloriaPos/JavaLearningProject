49df7be9e26f80f9e6d29a9c68959bc93de44353
GloriaPos <gloria.postovan@gmail.com>
2023-05-28 14:27:54 +0300

    Added the implementation of Zoo Service by using OOP

---
+++ src/main/java/com/postovan/zooclubservice/Animal.java
@@ -0,0 +1,18 @@
+package com.postovan.zooclubservice;
+
+public abstract class Animal {
+    private String name;
+
+    protected Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void eat();
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/postovan/zooclubservice/AnimalInterface.java
@@ -0,0 +1,7 @@
+package com.postovan.zooclubservice;
+
+public interface AnimalInterface {
+    // Toate clasele care implementeaza AnimalInterface va trebui sa faca override la makesound() (adica sa implementeze metoda)
+    public void makeSound();
+
+}
